5d53713e0eed55e9e3ddb182ef047b12f57b91e9
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-07-29 01:51:11 -0700

    Update fill-dates_function.R

--- src/fill-dates_function.R
+++ src/fill-dates_function.R
@@ -7,10 +7,10 @@ library(lubridate)
 
 
 # function definition: -------------
-fill_dates <- function(df, date_col, start_date, end_date){
+fill_dates <- function(df, date_id_col, start_date, end_date){
   # inputs: 
   # df is a dataframe 
-  # date_col is a column with date ids - e.g. "20190101" 
+  # date_id_col is a column with date ids - e.g. "20190101" 
   # start_date and end_date create the range of dates to "fill in"
   
   # create date range: 
@@ -20,12 +20,12 @@ fill_dates <- function(df, date_col, start_date, end_date){
   
   # "quote"/"capture" the value that the user inputs (the actual arg),
   # but don't evaluate it:
-  date_col <- enexpr(date_col)
+  date_id_col <- enexpr(date_id_col)
   
   # now join: 
   df <- 
     df %>% 
-    mutate(dates_fill = ymd(!!date_col)) %>% 
+    mutate(dates_fill = ymd(!!date_id_col)) %>% 
     full_join(df_full_dates) %>% 
     arrange(dates_fill)
   
@@ -37,12 +37,12 @@ fill_dates <- function(df, date_col, start_date, end_date){
 
 # test the function: ---------
 # create test df
-# df1 <- data.frame(this_col_has_dates = c("20190101", "20190104"), 
+# df1 <- data.frame(this_col_has_dates = c("20190101", "20190104"),
 #                   other_col = rnorm(2))
 # 
-# # fill in dates: 
-# df1 %>% 
-#   fill_dates(date_col = this_col_has_dates, 
-#              start_date = "2019-01-01", 
+# fill in dates: 
+# df1 %>%
+#   fill_dates(date_id_col = this_col_has_dates,
+#              start_date = "2019-01-01",
 #              end_date = "2019-01-07")
 
